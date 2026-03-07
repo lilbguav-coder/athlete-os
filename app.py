1f372797746660209a894808c5b86b7eb4909732
lilbguav-coder <lilbguav@gmail.com>
2026-03-07 19:05:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -95,6 +95,7 @@ class RecordManuel(Base):
     user_id = Column(Integer)
     nom_exo = Column(String)
     valeur_1rm = Column(Float)
+    
     Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 db = Session()
